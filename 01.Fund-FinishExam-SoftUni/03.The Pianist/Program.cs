28ed454baf76d953276f80e4195c8d8095399377
BlagoDamqnov <chessplayer89@abv.bg>
2022-03-23 20:29:47 +0200

    Add a gitIgnore file

---
+++ 01.Fund-FinishExam-SoftUni/03.The Pianist/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.The_Pianist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
