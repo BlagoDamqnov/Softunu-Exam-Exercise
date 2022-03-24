5f015ab2667272cf780b5e2bdb2b26164eabf487
BlagoDamqnov <chessplayer89@abv.bg>
2022-03-24 09:37:37 +0200

    Solve

---
+++ 01.Fund-FinishExam-SoftUni/02.Ad Astra/MatchCollerction.cs
@@ -0,0 +1,14 @@
+﻿namespace _02.Ad_Astra
+{
+    internal class MatchCollerction
+    {
+        private string text;
+        private string pattern;
+
+        public MatchCollerction(string text, string pattern)
+        {
+            this.text = text;
+            this.pattern = pattern;
+        }
+    }
+}
--- 01.Fund-FinishExam-SoftUni/02.Ad Astra/Program.cs
+++ 01.Fund-FinishExam-SoftUni/02.Ad Astra/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace _02.Ad_Astra
 {
@@ -6,7 +7,35 @@ namespace _02.Ad_Astra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string text = Console.ReadLine();
+            int totalCal = 0;
+            int neededDay = 0;
+
+            Regex regText = new Regex(@"(#|\|)+(?<name>[A-Z *a-z]+)\1(?<date>[\d]{2}\/[\d]{2}\/[\d]{2})\1(?<cal>[\d]+)\1");
+
+            MatchCollection match = regText.Matches(text);
+            
+            foreach (Match mathches in match)
+            {
+                string name = mathches.Groups["name"].Value;
+                string date = mathches.Groups["date"].Value;
+                int cal = int.Parse(mathches.Groups["cal"].Value);
+
+                totalCal += cal;
+            }
+
+            neededDay = totalCal / 2000;
+
+            Console.WriteLine($"You have food to last you for: {neededDay} days!");
+            foreach (Match mathches in match)
+            {
+                string name = mathches.Groups["name"].Value;
+                string date = mathches.Groups["date"].Value;
+                int cal = int.Parse(mathches.Groups["cal"].Value);
+
+                Console.WriteLine($"Item: {name}, Best before: {date}, Nutrition: {cal}");
+            }
+
         }
     }
 }
